69bb25b74e47edb9f87aab7881b6a9b880fa1bc2
Jacob Dresdale <15719920+jd4883@users.noreply.github.com>
2021-06-19 23:29:34 -0700

    cleanup in progress

--- resources.tf
+++ resources.tf
@@ -1,11 +1,11 @@
 resource "digitalocean_domain" "domain" {
-  name = var.domain
+  name       = var.domain
   ip_address = data.http.get_local_ip.body
 }
 
 resource "digitalocean_record" "record" {
   for_each = toset(var.a_records)
-  domain = var.domain
+  domain   = var.domain
   name     = each.value
   type     = "A"
   value    = digitalocean_domain.domain.ip_address
@@ -18,7 +18,7 @@ resource "digitalocean_record" "ns" {
   name   = "ns${count.index + 1}"
   type   = "NS"
   value  = "ns${count.index + 1}.${var.domain}."
-  ttl      = var.default_ttl
+  ttl    = var.default_ttl
 }
 
 resource "digitalocean_certificate" "domain" {
@@ -26,6 +26,7 @@ resource "digitalocean_certificate" "domain" {
   type = var.certificate_type
   domains = [
     var.domain,
+    join(".", ["*", var.domain]),
     #digitalocean_record.domain_wildcard.fqdn
     # TODO: this is a provider limitation, either a DO alternative or some kind of better way to handle this would be slick
     # TODO: if the cert can be managed elsewhere it can probably just get loaded into DO every time
